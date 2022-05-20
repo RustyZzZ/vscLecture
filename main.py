54fe7bd877a42c0fcda247096f3577bfda4b56f9
Rostyslav Diachuk <rdiachuk92@gmail.com>
2022-05-20 09:06:48 +0300

    change to vsc

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Visual Studio')
+    print_hi('Visual Studio Code')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
