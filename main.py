07702fe22b72c07f803f8ea3960d6688ca23ae64
Rostyslav Diachuk <rdiachuk92@gmail.com>
2022-05-20 09:23:55 +0300

    Changed to github

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Visual Studio Code')
+    print_hi('Github')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
